4206c3799c19f050788b43530c62f7459d7ff231
tianliang <tianliang@axon.com.cn>
2018-11-22 16:06:16 +0800

    接口403改为tips提示，不强制弹登窗口

--- src/api/exception.js
+++ src/api/exception.js
@@ -5,6 +5,8 @@ export default {
   ErrorMsgNotification (code, msg) {
     if (window.$globalHub.$route.path !== '/' && window.$globalHub.$route.path !== '/login' && codes.AUTH_ERROR.some(x => x === code)) {
       window.$globalHub.$store.commit('ADMIN_LOGIN', true)
+    } else if (code === 403) {
+      Notification.error({ title: '错误', message: '当前资源禁止访问！' })
     } else {
       Notification.error({ title: '错误', message: msg || '传输异常，请稍后再试！' })
     }
--- src/codes/apiResultCodes.js
+++ src/codes/apiResultCodes.js
@@ -1,6 +1,6 @@
 export default {
   SUCCESS: '000000', // 接口请求成功标识
   AUTH_ERROR: [ // 鉴权失败，需重新登录
-    401, 403
+    401
   ]
 }
